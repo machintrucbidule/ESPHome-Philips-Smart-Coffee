f9beaaa6c47ad5b5311d0d4615ab7b8147056fcc
machintrucbidule <75478334+machintrucbidule@users.noreply.github.com>
2024-02-28 03:49:55 +0100

    Update power.h

--- components/philips_coffee_machine/switch/power.h
+++ components/philips_coffee_machine/switch/power.h
@@ -6,7 +6,7 @@
 #include "../commands.h"
 
 #define MESSAGE_REPETITIONS 5
-#define POWER_TRIP_RETRY_DELAY 1000
+#define POWER_TRIP_RETRY_DELAY 100
 #define MAX_POWER_TRIP_COUNT 5
 
 namespace esphome
