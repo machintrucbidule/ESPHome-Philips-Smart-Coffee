4d30a0457af1300789b25c45b4a0461946d912d8
machintrucbidule <75478334+machintrucbidule@users.noreply.github.com>
2024-02-28 03:12:21 +0100

    Update philips_coffee_machine.cpp

--- components/philips_coffee_machine/philips_coffee_machine.cpp
+++ components/philips_coffee_machine/philips_coffee_machine.cpp
@@ -86,8 +86,9 @@ namespace esphome
             if (millis() - last_message_from_display_time_ > POWER_STATE_TIMEOUT)
             {
 #ifdef USE_SWITCH
+                ESP_LOGE(TAG, "Sent turn off, no message");
                 // Update power switches
-                for (philips_power_switch::Power *power_switch : power_switches_)
+                for (philips_power_switch::Power *power_switch : power_switches_){
                     power_switch->update_state(false);
 #endif
 
